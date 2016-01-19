c9c476a80174d2acc8a4c7db56dd8e89d81d8dd7
James Jin <etimes001@gmail.com>
2016-01-19 23:24:53 +1300

    create user table function before check table existing

--- src/Entity/UserRes.php
+++ src/Entity/UserRes.php
@@ -11,4 +11,31 @@ class UserRes extends resCommon
 
         $this->sqlManager->insert($this->sqlBuilder);
     }
+
+    public function createTable()
+    {
+       $query = "
+           CREATE TABLE `".$this->table."` (
+              `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+              `name` varchar(50) NOT NULL DEFAULT '',
+              `surname` varchar(50) NOT NULL DEFAULT '',
+              `email` varchar(254) NOT NULL DEFAULT '',
+              PRIMARY KEY (`id`)
+            ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;";
+
+        if (!$this->checkTable()){
+            $result = $this->dbConnect->executeSql($query);
+            return ($result) ? true : false;
+        }else{
+            return false;
+        }
+    }
+
+    private function checkTable(){
+        $query = "SHOW TABLES LIKE '".$this->table."'";
+        $this->sqlBuilder
+            ->setQuery($query);
+        $result = $this->sqlManager->fetchRow($this->sqlBuilder);
+        return ($result) ? true : false;
+    }
 }
