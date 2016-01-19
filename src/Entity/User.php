95a432eb24d9e8ab940037a3850552dd4ff678a4
James Jin <etimes001@gmail.com>
2016-01-20 00:41:52 +1300

    filter name and surnace for capitalised

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -1,6 +1,8 @@
 <?php
 namespace Catalyst\Entity;
 
+use Egulias\EmailValidator\EmailValidator;
+
 class User extends UserRes
 {
     public $table = 'user';
@@ -8,4 +10,46 @@ class User extends UserRes
     protected $name;
     protected $surname;
     protected $email;
+
+
+    public function filterName($name){
+        $this->name = ucfirst($name);
+        return $this->name;
+    }
+
+    public function filterSurname($surname){
+        $this->surname = ucfirst($surname);
+        return $this->surname;
+    }
+
+    public function filterEmail($email){
+        $emailResult =  $this->validEmail($email);
+        if ($emailResult['status']){
+            $this->email = strtolower($email);
+            return $this->email;
+        }else{
+            return $emailResult;
+        }
+    }
+
+    /**
+     * Validate Email Address Format
+     * @param $email
+     * @return array
+     */
+    private function validEmail($email){
+        $validator = new EmailValidator;
+
+        $result = array(
+            'status'=>true,
+        );
+
+        if (!$validator->isValid($email)) {
+            $result = array(
+                'msg'=>$email.' is not a legal format',
+                'status'=>false,
+            );
+        }
+        return $result;
+    }
 }
