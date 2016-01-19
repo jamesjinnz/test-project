f841c4f2349587af18e4a00544b8cfec0c1f7228
James Jin <etimes001@gmail.com>
2016-01-20 02:55:16 +1300

    check mysql mannual setting after dry_run and help

--- src/Library/console.php
+++ src/Library/console.php
@@ -57,91 +57,96 @@ class Console
             'description' => 'MySQL database'));
 
         $arguments->parse();
-
-        if ($arguments['help']) {
-            echo $arguments->getHelpScreen();
-            echo "\n\n";
-            die;
-        }else{
-            if (!$arguments['u'] && !$arguments['p'] && !$arguments['h']) {
-                if(!$this->app->dbConn){
-                    echo \cli\line("Warning: Database .env not setup, you also can put Mysql database info in the CLI. Please type --help for more info.");
-                    die;
-                }
-            }
-
-            if(!$this->app->dbConn){
-                if (!$arguments['u']){
-                    echo \cli\line("Error: Username Missing, -u Mysql Username");
-                    die;
-                }
-
-                if (!$arguments['p']){
-                    echo \cli\line("Error: Password Missing, -u Mysql Password");
-                    die;
-                }
-
-                if (!$arguments['h']){
-                    echo \cli\line("Error: Host Missing, -u Mysql Host");
-                    die;
-                }
-            }
-
-            if ($arguments['u'] || $arguments['p'] || $arguments['h']) {
-                if ($arguments['u'] && $arguments['p'] && $arguments['h']) {
-                    $dbSetting = new Setting();
-                    if(!$this->app->dbConn){
-                        if (!$arguments['d']){
-                            echo \cli\line("Error: Database Name Missing, -d Mysql Database Name");
-                            die;
-                        }
-                        $database = $arguments['d'];
-                    }else{
-                        $database = $dbSetting->getDatabase();
-                    }
-                    $dbConn =$dbSetting->reloadDbConfig($arguments['h'],$arguments['u'],$arguments['p'],$database);
-                    $this->dbConnect = $this->app->dbInit($dbConn);
-                }else{
-                    echo \cli\line("Error: You have to put Mysql host, user and password, please type --help for more detail");
-                    die;
-                }
-
-            }
-
-        }
         $this->menuProcess($arguments);
     }
 
     private function menuProcess(Arguments $arguments)
     {
         $csvHandler = new csvHandler();
-        $user = new User($this->dbConnect);
-        if ($arguments['dry_run']) {
-            if ($arguments['file']) {
-                $csvHandler->readFile($arguments['file'],$user,true);
-            }else{
-                echo \cli\line("Error: must used with the --file directive");
-            }
+
+        if ($arguments['help']) {
+            echo $arguments->getHelpScreen();
+            echo "\n\n";
+            die;
         }else{
-            if ($arguments['file']) {
-                $csvHandler->readFile($arguments['file'],$user,false);
-            }
-
-            if ($arguments['create_table']) {
-                $result = $user->createTable();
-                if ($result){
-                    $msg = "User table build now";
+            if ($arguments['dry_run']) {
+                if ($arguments['file']) {
+                    $this->dbMannualSetup($arguments);
+                    $user = new User($this->dbConnect);
+                    $csvHandler->readFile($arguments['file'],$user,true);
                 }else{
-                    $msg = "User table already existing";
+                    echo \cli\line("Error: must used with the --file directive");
                 }
-                echo \cli\line($msg);
-            }
+                die;
+            }else{
+                $this->dbMannualSetup($arguments);
+                $user = new User($this->dbConnect);
+                if ($arguments['file']) {
+                    $csvHandler->readFile($arguments['file'],$user,false);
+                }
 
-            if ($arguments['help']) {
-                echo $arguments->getHelpScreen();
-                echo "\n\n";
+                if ($arguments['create_table']) {
+                    $result = $user->createTable();
+                    if ($result){
+                        $msg = "User table build now";
+                    }else{
+                        $msg = "User table already existing";
+                    }
+                    echo \cli\line($msg);
+                    die;
+                }
+            }
+        }
+    }
+
+    /**
+     * @param Arguments $arguments
+     */
+    private function dbMannualSetup(Arguments $arguments)
+    {
+        if (!$arguments['u'] && !$arguments['p'] && !$arguments['h']) {
+            if (!$this->app->dbConn) {
+                echo \cli\line("Warning: Database .env not setup, you also can put Mysql database info in the CLI. Please type --help for more info.");
+                die;
             }
         }
 
+        if (!$this->app->dbConn) {
+            if (!$arguments['u']) {
+                echo \cli\line("Error: Username Missing, -u Mysql Username");
+                die;
+            }
+
+            if (!$arguments['p']) {
+                echo \cli\line("Error: Password Missing, -u Mysql Password");
+                die;
+            }
+
+            if (!$arguments['h']) {
+                echo \cli\line("Error: Host Missing, -u Mysql Host");
+                die;
+            }
+        }
+
+        if ($arguments['u'] || $arguments['p'] || $arguments['h']) {
+            if ($arguments['u'] && $arguments['p'] && $arguments['h']) {
+                $dbSetting = new Setting();
+                if (!$this->app->dbConn) {
+                    if (!$arguments['d']) {
+                        echo \cli\line("Error: Database Name Missing, -d Mysql Database Name");
+                        die;
+                    }
+                    $database = $arguments['d'];
+                } else {
+                    $database = $dbSetting->getDatabase();
+                }
+                $dbConn = $dbSetting->reloadDbConfig($arguments['h'], $arguments['u'], $arguments['p'], $database);
+                $this->dbConnect = $this->app->dbInit($dbConn);
+            } else {
+                echo \cli\line("Error: You have to put Mysql host, user and password, please type --help for more detail");
+                die;
+            }
+
+        }
     }
 }
